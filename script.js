65be6cfa5c0e5bc5563b53ed11e0b2dad8336e13
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-03-13 15:54:35 +0400

    add: cross finished tasks functionality
    also added some comments for more code readability

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newtask input").value.length == 0) {
     alert("Please Enter a Task");
   } else {
+    // Add Tasks Functionality
     document.querySelector("#tasks").innerHTML += `<div class="task"><span>${
       document.querySelector("#newtask input").value
     }</span><button class="delete"><i class="fa-solid fa-trash"></i></button></div>`;
 
+    // Delete Tasks Functionality
     var current_tasks = document.querySelectorAll(".delete");
-    for (var i = 0; 1 < current_tasks.length; i++) {
+    for (var i = 0; i < current_tasks.length; i++) {
       current_tasks[i].onclick = function () {
         this.parentNode.remove();
+      };
+    }
+
+    // Cross Finished Tasks Functionality
+    var tasks = document.querySelectorAll(".task");
+    for (var i = 0; i < tasks.length; i++) {
+      tasks[i].onclick = function () {
+        this.classList.toggle("completed");
       };
     }
   }
