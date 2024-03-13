96fa45c127413b6256af6129beb8c9c98d25db27
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-03-13 15:47:18 +0400

    add: delete tasks functionality

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ document.querySelector("#push").onclick = function () {
   } else {
     document.querySelector("#tasks").innerHTML += `<div class="task"><span>${
       document.querySelector("#newtask input").value
-    }</span><button class="delete"><i class="far fa-trash-alt"></i></button></div>`;
+    }</span><button class="delete"><i class="fa-solid fa-trash"></i></button></div>`;
+
+    var current_tasks = document.querySelectorAll(".delete");
+    for (var i = 0; 1 < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
   }
 };
